78d5b3f929ddf376658f1b82e68ac97bc5e40495
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-06-08 09:25:44 +0200

    Adding more tests

---
+++ tests/search/test_engine.py
@@ -0,0 +1,54 @@
+import unittest
+from search import TagFilter
+from indexer import ProductIndexer
+from models import Product
+
+
+class EngineTest(unittest.TestCase):
+
+    def setUp(self):
+        # Rome
+        self.lat = 41.8723889
+        self.lng = 12.48018019999995
+        self.items = [
+            self._create_product(41.876748, 12.469616, '1'),
+            self._create_product(41.878522, 12.489014, '2'),  # 1km
+            self._create_product(
+                41.863123, 12.500882, '3', tags=['hej'])  # 2km
+        ]
+
+        self.searcher = ProductIndexer(index_name="")
+        for item in self.items:
+            self.searcher.index_product(item)
+
+    def _create_product(self, lat, lng, delta_id, tags=None):
+        p = Product('567d07d533d541dd8ad4753622e76243' + delta_id,
+                    '4aa53e646bf84faca9a76c020b0682de',
+                    'testing and testing',
+                    0.99,
+                    0.66,
+                    lng,
+                    lat)
+        if tags:
+            p.tags = tags
+
+        return p
+
+    def test_find_500m(self):
+        result = self.searcher.find(self.lng, self.lat, 500)
+        self.assertEqual(len(result), 0)
+
+    def test_find_1km(self):
+        result = self.searcher.find(self.lng, self.lat, 1001)
+        self.assertEqual(len(result), 2)
+
+    def test_find_2km(self):
+        result = self.searcher.find(self.lng, self.lat, 2001)
+        self.assertEqual(len(result), 3)
+
+    def test_find_tags(self):
+        result = self.searcher.find(
+            self.lng, self.lat, 2001, filters=[TagFilter(['hej'])])
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].id, self.items[2].id)
--- tests/search/test_filters.py
+++ tests/search/test_filters.py
@@ -41,3 +41,43 @@ class RadiusFilterTest(unittest.TestCase):
         self.assertEqual(result[0].lng, 12.469616)
         self.assertEqual(result[1].lat, 41.878522)
         self.assertEqual(result[2].lat, 41.863123)
+
+
+class TagFilterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.items = [Product('567d07d533d541dd8ad4753622e76243',
+                              '4aa53e646bf84faca9a76c020b0682de',
+                              'testing and testing',
+                              0.99,
+                              0.66,
+                              18.06061237898499,
+                              59.33265972650577,
+                              tags=['hej', 'hopp']),
+                      Product('567d07d533d541dd8ad4753622e76241',
+                              '4aa53e646bf84faca9a76c020b0682de',
+                              'testing and testing 2',
+                              0.99,
+                              0.66,
+                              18.06061237898499,
+                              59.33265972650577,
+                              tags=['hej', 'mopp'])]
+
+    def test_both_matched(self):
+        f = TagFilter(['hej'])
+        result = f.apply(self.items)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].id, '567d07d533d541dd8ad4753622e76243')
+        self.assertEqual(result[1].id, '567d07d533d541dd8ad4753622e76241')
+
+    def test_matched_hopp(self):
+        f = TagFilter(['hopp'])
+        result = f.apply(self.items)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].id, '567d07d533d541dd8ad4753622e76243')
+
+    def test_matched_mopp(self):
+        f = TagFilter(['mopp'])
+        result = f.apply(self.items)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].id, '567d07d533d541dd8ad4753622e76241')
