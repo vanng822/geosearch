de2568dd1feec001098574e28848ca0ef8bca475
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-06-14 11:10:50 +0200

    Redefine sort function to a generic one. Some overhead but can use for any property

--- search/sorting.py
+++ search/sorting.py
@@ -1,4 +1,10 @@
 
+def sort_by_property(prop):
+    def _sort_by_property(items):
+        return sorted(items,
+                      key=lambda item: getattr(item, prop),
+                      reverse=True)
 
-def sort_by_popularity(items):
-    return sorted(items, key=lambda product: product.popularity, reverse=True)
+    return _sort_by_property
+
+sort_by_popularity = sort_by_property('popularity')
