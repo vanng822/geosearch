2f29db7bd30b3db40f6577a4718623f6cd97a5cf
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-06-14 17:16:44 +0200

    Make it possible to control reverse

--- search/sorting.py
+++ search/sorting.py
@@ -1,10 +1,10 @@
 
-def sort_by_property(prop):
+def sort_by_property(prop, reverse=False):
     def _sort_by_property(items):
         return sorted(items,
                       key=lambda item: getattr(item, prop),
-                      reverse=True)
+                      reverse=reverse)
 
     return _sort_by_property
 
-sort_by_popularity = sort_by_property('popularity')
+sort_by_popularity = sort_by_property('popularity', reverse=True)
